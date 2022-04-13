a821603183a3c91eb90262b99e94052cd2d1a344
Alexandr Timoshchuk <92025957+tmshchk@users.noreply.github.com>
2022-04-13 14:02:54 +0300

    add default arrays, improved random

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
-let players = [];
-let teams = [];
+let players = [
+  {
+    name: 'Игрок №1',
+    team: [null, null, null, null, null, null],
+  },
+  {
+    name: 'Игрок №2',
+    team: [null, null, null, null, null, null],
+  },
+  {
+    name: 'Игрок №3',
+    team: [null, null, null, null, null, null],
+  },
+  {
+    name: 'Игрок №4',
+    team: [null, null, null, null, null, null],
+  },
+  {
+    name: 'Игрок №5',
+    team: [null, null, null, null, null, null],
+  },
+  {
+    name: 'Игрок №6',
+    team: [null, null, null, null, null, null],
+  },
+  {
+    name: 'Игрок №7',
+    team: [null, null, null, null, null, null],
+  },
+  {
+    name: 'Игрок №8',
+    team: [null, null, null, null, null, null],
+  },
+];
+let teams = [
+  'Манчестер Сити',
+  'ПСЖ',
+  'Манчестер Юнайтед',
+  'Ювентус',
+  'Реал Мадрид',
+  'Челси',
+  'Тотенхэм',
+  'Ливерпуль',
+  'Бавария',
+  'Барселона',
+  'Боруссия',
+  'Севилья',
+  'Лейпциг',
+  'Милан',
+  'Интер',
+  'Атлетико',
+];
 
-let p = document.createElement('p');
-let divResult = document.getElementById('list-data');
-
-// отслеживаем добавление игрока и добавляем значение в массив
-document.getElementById('form-player').addEventListener('submit', function (e) {
-  e.preventDefault();
-  const playerNameValue = document.getElementById('input-player').value;
-  const playerNameForm = document.getElementById('form-player');
-  console.log(playerNameValue);
-  if (playerNameValue !== null) {
-    players.push(playerNameValue);
-    playerNameForm.reset();
-    p.appendChild(document.createTextNode(`${playerNameValue} `));
-    divResult.appendChild(p);
-  }
-  console.log(players);
-});
-
-// отслеживаем добавление команд и добавляем значение в массив
-document.getElementById('form-team').addEventListener('submit', function (e) {
-  e.preventDefault();
-  const teamNameValue = document.getElementById('input-team').value;
-  const teamNameForm = document.getElementById('form-team');
-  console.log(teamNameValue);
-  if (teamNameValue !== null) {
-    teams.push(teamNameValue);
-    teamNameForm.reset();
-    p.appendChild(document.createTextNode(`${teamNameValue} `));
-    divResult.appendChild(p);
-  }
-  console.log(teams);
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 // возвращаем рандомное значение из массива
 Array.prototype.sample = function () {
@@ -48,33 +69,63 @@ function shuffle(array) {
 }
 
 // отслеживанием нажатие кнопки рандома
-document.getElementById('button-random').addEventListener('click', function (e) {
+document.getElementById('button-random').addEventListener('click', async function (e) {
   e.preventDefault();
 
   let divResult = document.getElementById('result');
+  divResult.innerHTML = 'Распределяю команды по игрокам...';
+
+  await sleep(1000);
 
   divResult.innerHTML = '';
 
-  cloneTeams = teams.slice();
-  clonePlayers = players.slice();
+  for (let i = 0; i < 6; i++) {
+    const div = document.createElement('div');
+    div.className = 'flex flex-col max-w-xs p-6 mb-6 border border-slate-200 rounded';
+    const h2 = document.createElement('h2');
 
-  if (cloneTeams.length < clonePlayers.length) {
-    return alert('Команд меньше, чем игроков. Распределить невозможно. Добавьте ещё команд.');
-  }
+    h2.appendChild(document.createTextNode(`Тур №${i + 1}`));
+    h2.className = 'text-lg text-slate-600 mb-4 grow';
+    div.appendChild(h2);
 
-  i = 0;
+    cloneTeams = teams.slice();
+    clonePlayers = players.slice();
 
-  while (i < players.length) {
-    team = cloneTeams.sample();
-    console.log(players[i] + ' - ' + team);
-    let p = document.createElement('p');
-    p.appendChild(document.createTextNode(`${players[i]} — ${team}`));
-    divResult.appendChild(p);
-    i++;
-    let myIndex = cloneTeams.indexOf(team);
-    if (myIndex !== -1) {
-      cloneTeams.splice(myIndex, 1);
+    const indexTeam = i;
+
+    for (let i = 0; i < players.length; i++) {
+      let team = cloneTeams.sample();
+
+      if (clonePlayers[i].team[indexTeam] === null) {
+        if (players[i].team.includes(team, 0) === true) {
+          do {
+            let newTeam = cloneTeams.sample();
+            team = newTeam;
+          } while (players[i].team.includes(team, 0) === true);
+          clonePlayers[i].team[indexTeam] = team;
+        } else {
+          clonePlayers[i].team[indexTeam] = team;
+        }
+      }
+
+      let p = document.createElement('p');
+
+      p.appendChild(document.createTextNode(`${players[i].name}`));
+      p.className = 'text-base text-slate-500 mb-4';
+      div.appendChild(p);
+
+      p.appendChild(document.createTextNode(` — ${clonePlayers[i].team[indexTeam]}`));
+      p.className = 'text-base text-slate-500 mb-4';
+      div.appendChild(p);
+
+      let myIndex = cloneTeams.indexOf(team);
+      if (myIndex !== -1) {
+        cloneTeams.splice(myIndex, 1);
+      }
+
+      shuffle(cloneTeams);
     }
-    shuffle(cloneTeams);
+
+    divResult.appendChild(div);
   }
 });
