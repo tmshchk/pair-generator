c50dc57e01f9fb7d8cf2bf32dd079a0704323b7d
Alexandr Timoshchuk <ingword@gmail.com>
2022-05-06 15:00:53 +0300

    update readme, fix logic

--- random.js
+++ random.js
@@ -1,60 +1,3 @@
-let players = [
-  {
-    name: 'Игрок №1',
-    team: [null, null, null, null, null, null],
-  },
-  {
-    name: 'Игрок №2',
-    team: [null, null, null, null, null, null],
-  },
-  {
-    name: 'Игрок №3',
-    team: [null, null, null, null, null, null],
-  },
-  {
-    name: 'Игрок №4',
-    team: [null, null, null, null, null, null],
-  },
-  {
-    name: 'Игрок №5',
-    team: [null, null, null, null, null, null],
-  },
-  {
-    name: 'Игрок №6',
-    team: [null, null, null, null, null, null],
-  },
-  {
-    name: 'Игрок №7',
-    team: [null, null, null, null, null, null],
-  },
-  {
-    name: 'Игрок №8',
-    team: [null, null, null, null, null, null],
-  },
-];
-let teams = [
-  'Манчестер Сити',
-  'ПСЖ',
-  'Манчестер Юнайтед',
-  'Ювентус',
-  'Реал Мадрид',
-  'Челси',
-  'Тотенхэм',
-  'Ливерпуль',
-  'Бавария',
-  'Барселона',
-  'Боруссия',
-  'Севилья',
-  'Лейпциг',
-  'Милан',
-  'Интер',
-  'Атлетико',
-];
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 // возвращаем рандомное значение из массива
 Array.prototype.sample = function () {
   return this[Math.floor(Math.random() * this.length)];
@@ -73,58 +16,57 @@ document.getElementById('button-random').addEventListener('click', async functio
   e.preventDefault();
 
   let divResult = document.getElementById('result');
-  divResult.innerHTML = 'Распределяю команды по игрокам...';
-
-  await sleep(1000);
-
   divResult.innerHTML = '';
 
-  for (let i = 0; i < 6; i++) {
-    const div = document.createElement('div');
-    div.className = 'flex flex-col max-w-xs p-6 mb-6 border border-slate-200 rounded';
-    const h2 = document.createElement('h2');
+  let firstListValues = document.getElementById('firstList').value;
+  let secondListValues = document.getElementById('secondList').value;
+  let tempFirstListArray = firstListValues.split('\n');
+  let tempSecondListArray = secondListValues.split('\n');
 
-    h2.appendChild(document.createTextNode(`Тур №${i + 1}`));
-    h2.className = 'text-lg text-slate-600 mb-4 grow';
-    div.appendChild(h2);
+  if (tempFirstListArray.length > tempSecondListArray.length) {
+    alert('Добавьте данных: второй список должен быть больше или равен первому.');
+    return;
+  }
 
-    cloneTeams = teams.slice();
-    clonePlayers = players.slice();
-
-    const indexTeam = i;
-
-    for (let i = 0; i < players.length; i++) {
-      let team = cloneTeams.sample();
-
-      if (clonePlayers[i].team[indexTeam] === null) {
-        if (players[i].team.includes(team, 0) === true) {
-          do {
-            let newTeam = cloneTeams.sample();
-            team = newTeam;
-          } while (players[i].team.includes(team, 0) === true);
-          clonePlayers[i].team[indexTeam] = team;
-        } else {
-          clonePlayers[i].team[indexTeam] = team;
-        }
-      }
-
-      let p = document.createElement('p');
-
-      p.appendChild(document.createTextNode(`${players[i].name}`));
-      p.className = 'text-base text-slate-500 mb-4';
-      div.appendChild(p);
-
-      p.appendChild(document.createTextNode(` — ${clonePlayers[i].team[indexTeam]}`));
-      p.className = 'text-base text-slate-500 mb-4';
-      div.appendChild(p);
-
-      let myIndex = cloneTeams.indexOf(team);
-      if (myIndex !== -1) {
-        cloneTeams.splice(myIndex, 1);
-      }
-
-      shuffle(cloneTeams);
+  let firstListArray = [];
+  for (let i = 0; i < tempFirstListArray.length; i++) {
+    if (tempFirstListArray[i] !== '') {
+      firstListArray.push(tempFirstListArray[i]);
     }
+  }
+
+  let secondListArray = [];
+  for (let i = 0; i < tempSecondListArray.length; i++) {
+    if (tempSecondListArray[i] !== '') {
+      secondListArray.push(tempSecondListArray[i]);
+    }
+  }
+
+  let cloneFirstList = firstListArray.slice();
+  let cloneSecondList = secondListArray.slice();
+
+  const div = document.createElement('div');
+  div.className = 'flex flex-col max-w-xs p-4 mb-6';
+
+  for (let i = 0; i < firstListArray.length; i++) {
+    let randomValue = cloneSecondList.sample();
+
+    let p = document.createElement('p');
+
+    p.appendChild(document.createTextNode(`${firstListArray[i]}`));
+    p.className = 'text-base text-slate-500';
+    div.appendChild(p);
+
+    p.appendChild(document.createTextNode(`: ${randomValue}`));
+    p.className = 'text-base text-slate-500';
+    div.appendChild(p);
+
+    let myIndex = cloneSecondList.indexOf(randomValue);
+    if (myIndex !== -1) {
+      cloneSecondList.splice(myIndex, 1);
+    }
+
+    shuffle(cloneSecondList);
 
     divResult.appendChild(div);
   }
